91d7dff244f287df0b6d396d163026650a9e78c6
Habib Hussain <habibhussain13@hotmail.com>
2017-03-14 15:09:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 //Problem: User interaction doesn't provide desired results.
 //Solution: Add interactivity so user can among tasks.
+//First, I'll add an event handler to the add button to add new tasks.
+//Secondly, the new added task has to be shown on the to-do list part.
+//Be able to edit and delete tasks. add event handlers for both buttons.
+//Add completed tasks to the completed task list.
